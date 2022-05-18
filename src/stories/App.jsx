df31d0dc78525112ddd0c70fa84fb2a1a3a0c614
Hanna <petrakovahanna@gmail.com>
2022-05-18 03:39:45 +0200

    overflow-x scrolls

--- src/stories/App.jsx
+++ src/stories/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./app.css";
 import { Carta } from "./Carta";
 import { Titulo } from "./Titulo";
+import PropTypes from "prop-types";
 
-function App({ args, data }) {
+function App({ args, data, direction }) {
   return (
     <div className="App">
       <Titulo label={"Mis regalos"} />
-      <section className="responsive-box">
+      <section className={`responsive-box--${direction}`}>
         {data.map((obj, index) => (
           <Carta {...args} label={obj} key={index} />
         ))}
@@ -15,5 +16,13 @@ function App({ args, data }) {
     </div>
   );
 }
+
+App.PropTypes = {
+  direction: PropTypes.oneOf(["row", "column"]),
+};
+
+App.defaultProps = {
+  direction: "row",
+};
 
 export default App;
--- src/stories/App.stories.jsx
+++ src/stories/App.stories.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import App from "./App";
-import { Carta } from "./Carta";
-import { Titulo } from "./Titulo";
 
 export default {
   title: "Global/App",
@@ -15,12 +13,6 @@ export default {
 };
 ///plantillaCarta recibe los datos que le paso a los argumentos. estos argumentos los imprime el Carta.jsx según lo que yo le pida
 const PlantillaCarta = ({ data, args }) => <App {...args} data={data} />;
-// export const Size = Plantilla.bind({});
-// Size.args = {
-//   label: "Escribe tu título",
-//   size: "sm",
-//   desc: "Descripcion",
-// };
 
 export const Lista = PlantillaCarta.bind({});
 Lista.args = {
@@ -37,6 +29,16 @@ Lista.args = {
     },
     {
       title: "Friends",
+      desc: "Lista de regalos para el grupo Friends",
+      tags: ["Arte", "Musica"],
+    },
+    {
+      title: "Friends 2",
+      desc: "Lista de regalos para el grupo Friends",
+      tags: ["Arte", "Musica"],
+    },
+    {
+      title: "Friends 3",
       desc: "Lista de regalos para el grupo Friends",
       tags: ["Arte", "Musica"],
     },
